6c27e9a860e5100388d2241a43ac9e137ba3866d
Виринский Кирилл <virinskij_ki_25@student.grsu.by>
2026-05-08 21:06:30 +0300

    Написал прототип для скачивания файлов с сайта

---
+++ ConsoleApplication1/Downloader.cpp
@@ -0,0 +1,37 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <urlmon.h>
+
+#pragma comment(lib, "urlmon.lib")
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	const char* places[] = {
+		"C:\\Test\\name.basics.tsv.gz",
+		"C:\\Test\\title.akas.tsv.gz",
+		"C:\\Test\\title.crew.tsv.gz",
+		"C:\\Test\\title.episode.tsv.gz",
+		"C:\\Test\\title.principals.tsv.gz",
+		"C:\\Test\\title.ratings.tsv.gz"};
+	const char* url[] = { 
+		"https://datasets.imdbws.com/name.basics.tsv.gz",
+		"https://datasets.imdbws.com/title.akas.tsv.gz",
+		"https://datasets.imdbws.com/title.crew.tsv.gz",
+		"https://datasets.imdbws.com/title.episode.tsv.gz",
+		"https://datasets.imdbws.com/title.principals.tsv.gz",
+		"https://datasets.imdbws.com/title.ratings.tsv.gz"};
+
+	for (int i = 0; i < 6; i++) {
+		HRESULT result = URLDownloadToFileA(NULL, url[i], places[i], 0, NULL);
+
+		if (result == S_OK) {
+			cout << "Обновление произведено успешно" << endl;
+		}
+		else {
+			cout << "Ошибка обновления" << endl;
+		}
+	}
+	return 67;
+}
