66ecd055d84ef99dbc150218284d5018f35e3fed
Виринский Кирилл <virinskij_ki_25@student.grsu.by>
2026-05-12 16:54:36 +0300

    Написал финальную версию загрузочной части. Одномерные массивы заменил на список map, сделал передачу значений в функцию через key/value.

--- ConsoleApplication1/Downloader.cpp
+++ ConsoleApplication1/Downloader.cpp
@@ -1,6 +1,7 @@
 ﻿#include <windows.h>
 #include <iostream>
 #include <urlmon.h>
+#include <map>
 
 #pragma comment(lib, "urlmon.lib")
 
@@ -8,29 +9,27 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "");
-	const char* places[] = {
-		"C:\\Test\\name.basics.tsv.gz",
-		"C:\\Test\\title.akas.tsv.gz",
-		"C:\\Test\\title.crew.tsv.gz",
-		"C:\\Test\\title.episode.tsv.gz",
-		"C:\\Test\\title.principals.tsv.gz",
-		"C:\\Test\\title.ratings.tsv.gz"};
-	const char* url[] = { 
-		"https://datasets.imdbws.com/name.basics.tsv.gz",
-		"https://datasets.imdbws.com/title.akas.tsv.gz",
-		"https://datasets.imdbws.com/title.crew.tsv.gz",
-		"https://datasets.imdbws.com/title.episode.tsv.gz",
-		"https://datasets.imdbws.com/title.principals.tsv.gz",
-		"https://datasets.imdbws.com/title.ratings.tsv.gz"};
+	map<string, string> files
+	{
+		{"C:\\Test\\name.basics.tsv.gz", "https://datasets.imdbws.com/name.basics.tsv.gz"},
+		{"C:\\Test\\title.akas.tsv.gz", "https://datasets.imdbws.com/title.akas.tsv.gz"},
+		{"C:\\Test\\title.crew.tsv.gz", "https://datasets.imdbws.com/title.crew.tsv.gz"},
+		{"C:\\Test\\title.episode.tsv.gz", "https://datasets.imdbws.com/title.episode.tsv.gz"},
+		{"C:\\Test\\title.principals.tsv.gz", "https://datasets.imdbws.com/title.principals.tsv.gz"},
+		{"C:\\Test\\title.ratings.tsv.gz", "https://datasets.imdbws.com/title.ratings.tsv.gz"}
+	};
+	for (const auto& file : files)
+	{
+		const string& place = file.first;
+		const string& url = file.second;
 
-	for (int i = 0; i < 6; i++) {
-		HRESULT result = URLDownloadToFileA(NULL, url[i], places[i], 0, NULL);
+		HRESULT result = URLDownloadToFileA(NULL, url.c_str(), place.c_str(), 0, NULL);
 
 		if (result == S_OK) {
 			cout << "Обновление произведено успешно" << endl;
 		}
 		else {
-			cout << "Ошибка обновления" << endl;
+			cout << "Ошибка обновления: " << place << "Код: " << result << endl;
 		}
 	}
 	return 67;
